f31481538c0300b5bb7a5af97dfc2511aa00e56b
Stefan Kovarbasic <kovar.zmaj@gmail.com>
2020-05-01 16:53:13 +0200

    Editing token

--- src/Communicators.js
+++ src/Communicators.js
@@ -4,12 +4,13 @@ class Communicators {
 
 	
 	static githubURL = 'https://api.github.com/search/users?q=';
-	static singleUserURL= 'https://api.github.com/users/';
+	static singleUserURL = 'https://api.github.com/users/';
+	static token = "23739f0b77e2b66c92dae1f49401e78ffc57956e"
 
 	static Fetch = (searchedTerm, page) => {
 		return axios(`${Communicators.githubURL}${searchedTerm}+in:login+type:user&per_page=10&page=${page}`, {
 			headers: {
-				authorization: "token 99dcf2bc2a64aa568ef595dd2998c08443a8c011"
+				authorization: "token " + this.token
 			  }
 		})
 	}
@@ -17,7 +18,7 @@ class Communicators {
 	static FetchSingleUser = searchedTerm => {
 		return axios(`${Communicators.singleUserURL}${searchedTerm}`, {
 			headers: {
-				authorization: "token 99dcf2bc2a64aa568ef595dd2998c08443a8c011"
+				authorization: "token " + this.token,
 			  }
 		})
 	}
@@ -25,7 +26,7 @@ class Communicators {
 	static FetchReposData = selectedUser => {
 		return axios(`${Communicators.singleUserURL}${selectedUser}/repos`, {
 			headers: {
-				authorization: "token 99dcf2bc2a64aa568ef595dd2998c08443a8c011"
+				authorization: "token " + this.token
 			  }
 		})
 	}
